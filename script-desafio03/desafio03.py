febf0d4280414e791426b003ee877b743ba08041
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2024-10-18 13:27:09 -0300

    commit desafio03.py
    Com esse exercício aprendemos que em python precisamos aplicar o argumento primitivo int a variável e para printar esse resultdo sem erro aplicamos o .format

---
+++ script-desafio03/desafio03.py
@@ -0,0 +1,7 @@
+print ('vamos brincar de somar?')
+
+n1 = int (input ('Digite o primeiro número: '))
+n2 = int (input ('digite o seundo número: '))
+resultado  = n1+n2
+
+print (' A soma de {} e  {} deu o total de {} '.format(n1, n2, resultado))
