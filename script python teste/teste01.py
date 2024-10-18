09549621f69d37d107b2896d187cf1ad6b4c50cf
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2024-10-18 11:12:44 -0300

    commit script teste01.py
    Nesse script empregamos a função print, input que tem por finalidade, captar dados digitados e imprimi-los.  Assim como usar vírgula  e o simbolo  ( + ) positivo no momento de imprimir os dados.

---
+++ script python teste/teste01.py
@@ -0,0 +1,8 @@
+nome = input ('Qual seu nome? ')
+idade = input ('Qual sua idade?')
+peso = input ('Qual seu peso?')
+altura = input ('Qual sua altura?')
+jogo = input ('Qual seu RPG favorito?')
+
+print (nome, idade, peso, altura, jogo)
+
