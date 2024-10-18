78ed8374455d33c77ad550a7748fb5592d45195b
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2024-10-18 11:57:29 -0300

    commit desafio02.py
    Pegando os dados de dia, mês e ano de nascimento e trzendo isso em tela. apenas uma bincadeira de manipular dados.

---
+++ script-desafio02/desafio02.py
@@ -0,0 +1,6 @@
+print ('Oi insira os dados solicitados abaixo, com o dia , mês e ano de nascimento')
+dia = input ('Dia: ')
+mes = input ('Mês: ')
+ano = input ('Ano: ' )
+
+print ('Vôce nasceu no dia ', dia,' de/do ' , mes, ' de ' , ano, ' ,tá coreto?')
